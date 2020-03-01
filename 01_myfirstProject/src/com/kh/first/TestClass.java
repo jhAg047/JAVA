8e057dc295f7d96b9d84b270e216ac116859d60f
jhAg047 <jhag047@gmail.com>
2020-03-01 21:41:57 +0900

    01_start

---
+++ 01_myfirstProject/src/com/kh/first/TestClass.java
@@ -0,0 +1,18 @@
+package com.kh.first;
+
+public class TestClass { //명령어들은 다 클래스 안에 있어야함  //클래스
+	public static void main(String[] args) { //메소드
+		System.out.println("Hello World"); //출력하는 문장
+		// 한 줄 주석
+		/*  여러줄 주석
+		 여기도 주석인가?
+		 아 주석이 아니네
+		 그럼 여러 줄 주석은 어떻게 한담
+		 */
+		/*
+		 * 주석을 씁니다
+		 * 오늘은 첫 수업
+		 * */
+		}
+}
+
